d2704c655b2f8d889fcd2ee2295cb18787970c5e
Richard Mills <scripts.richard@gmail.com>
2017-07-03 20:28:31 -0600

    Added comments and doc strings.

--- arduino_display.py
+++ arduino_display.py
@@ -11,7 +11,25 @@ ARDUINO = 'Arduino Mega 2560'  # Arduino to be used
 
 
 class Package:
+    """Creates a serial connection to the arduino and prepares a "package" to
+    be delivered. Houses all functions used to create the package.
+
+    Package layout:
+    <time><date><temp><weather><cpu><ram>
+    hhmmDDMMWCP.URA.M!
+    h - hour
+    m - minute
+    D - day
+    M - month
+    CP.U - cpu as a decimal
+    RA.M - ram as a decimal
+    ! - denotes the end of the package
+
+    * - as the first character is the script ending signal.
+    """
+
     def __init__(self, target, port=9600):
+        """target - arduino description, port - baudrate."""
         self.target = target
         self.prev_wthr_time = None
         self.wthr_package = '  _'
@@ -22,6 +40,7 @@ class Package:
         self.ser = serial.Serial(self.device, port)
 
     def get_date_and_time_package(self):
+        """Update the date and time and convert to a string for the package."""
         self.time = datetime.datetime.now()
         date_time = self.time.strftime('%H%M%w%m%d')
         if date_time[-2] == '0':
@@ -29,11 +48,17 @@ class Package:
         return date_time
 
     def time_to_get_weather(self, interval=1800):
+        """Decide to get weather based upon a set interval (in seconds)."""
         if self.prev_wthr_time is None:
             return True
         return (self.time - self.prev_wthr_time).seconds >= interval
 
     def get_weather(self):
+        """Use Open Weather Map to get location weather, extract temperature
+        and weather description, use status_map to map weather description to
+        the appropriate character to be used be the weather font.
+        """
+
         status_map = {'clear sky': 'J',
                       'few clouds': 'F',
                       'scattered clouds': 'F',
@@ -48,6 +73,7 @@ class Package:
 
         print('Making weather request...')
 
+        # Try and except to avoid exception on connection failure
         try:
             obs = self.owm.weather_at_place('Edmonton, Canada')
             w = obs.get_weather()
@@ -67,10 +93,14 @@ class Package:
 
             self.wthr_package = temp + status_char
 
+        # TODO: Modify for specific exception
         except:
             print('OWM connection error...')
 
     def get_cpu_and_ram_package(self):
+        """Use psutil to retrieve cpu and ram usage as a percentage and return
+        together as a concatenated string.
+        """
         cpu = str(psutil.cpu_percent())
         if len(cpu) == 3:
             cpu = ' ' + cpu
@@ -82,6 +112,9 @@ class Package:
         return cpu + ram
 
     def create_and_send(self):
+        """Create the package be calling the appropriate class functions,
+        terminate ('!') and send via serial connection to arduino.
+        """
         package = self.get_date_and_time_package()
         if self.time_to_get_weather():
             self.get_weather()
@@ -92,9 +125,11 @@ class Package:
         self.ser.write(package.encode())
 
     def send_script_end_signal(self):
+        """Send single character ('*') to singal script end."""
         self.ser.write('*'.encode())
 
     def find_device(self):
+        """Determine the location of the arduino based upon a description."""
         ports = list(serial.tools.list_ports.comports())
         for p in ports:
             if p.description == ARDUINO:
